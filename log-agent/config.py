687830e759a4c61a545eac7f1f384729bd987cab
chris-carstens <ccarstens256@gmail.com>
2025-04-17 04:14:16 +0200

    feat: add draft of rl_agent

--- log-agent/config.py
+++ log-agent/config.py
@@ -7,5 +7,9 @@ CONFIG = {
     },
     'prometheus': {
         'url': os.getenv('PROMETHEUS_URL', 'http://localhost:9090'),
-    }
+    },
+    'rl_agent': {
+        'url': os.getenv('RL_AGENT_URL'),
+        'response_time_threshold': os.getenv('RESPONSE_TIME_THRESHOLD', 0.5),
+    },
 }
--- log-agent/prometheus_client.py
+++ log-agent/prometheus_client.py
@@ -23,17 +23,18 @@ class PrometheusClient:
             return None
 
     def get_pod_cpu_usage(self):
+        # TODO: CHECK THE TIME, NOW IS WITH 1M
         """Get CPU usage for all Flask pods directly from the query without calculations"""
         query = 'sum by (pod) (rate(container_cpu_usage_seconds_total{pod=~"flask.*"}[1m])) * 100'
         result = self.query(query)
-        
+
         if not result or result.get('status') != 'success':
             return {}
-        
+
         cpu_usage = {}
         for metric in result.get('data', {}).get('result', []):
             pod_name = metric.get('metric', {}).get('pod', 'unknown')
             value = float(metric.get('value', [0, 0])[1])
             cpu_usage[pod_name] = value
-            
+
         return cpu_usage
