bf01216e9deb71f4e1f9da70ec0eda3c5c44ceed
chris-carstens <ccarstens256@gmail.com>
2025-09-18 13:44:28 +0200

    delete FIGARO old folder

--- Figaro/src/__init__.py
+++
@@ -1,5 +0,0 @@
-from .custom_environment import CustomEnvironment
-
-from ray.tune.registry import register_env
-
-register_env("CustomEnvironment", lambda config: CustomEnvironment(config))
--- Figaro/src/production_agents/DQN/__init__.py
+++
@@ -1,6 +0,0 @@
-import os
-import sys
-
-
-_CUSTOM_DQN_PATH = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(_CUSTOM_DQN_PATH)
