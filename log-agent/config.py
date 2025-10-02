224107aae60e6c2bdd2eb4174b76f614106a7b83
Christian Carstens <ctcarstens@uc.cl>
2025-10-02 10:47:13 +0000

    update hyper params

--- log-agent/config.py
+++ log-agent/config.py
@@ -8,17 +8,17 @@ CONFIG = {
         'url': os.getenv('PROMETHEUS_URL', 'http://localhost:9090'),
     },
     'rl_agent': {
-        "max_workload": 3,
+        "max_workload": 2,
         'pressure_clip_value': 3.0,
         'queue_length_dominant_clip_value': 10.0,
         'max_n_replicas': 5,
         'response_time_threshold': {
             "flask-app-1": 1.1,
-            "flask-app-2": 0.8,
+            "flask-app-2": 0.75,
         },
         "demand": {
-            "flask-app-1": 0.7536123180389404,
-            "flask-app-2": 0.5587082195281982,
+            "flask-app-1": 0.712,
+            "flask-app-2": 0.561,
         },
     },
     'scale_kubernetes': {
